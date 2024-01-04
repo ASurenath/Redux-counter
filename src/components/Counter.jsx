7854b2b6a6d449cce812947da6633ab36a338522
Anagha <anaghalakshmeeka@gmail.com>
2024-01-04 10:28:48 +0530

    second commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -19,17 +19,17 @@ function Counter() {
     }
     return (
         <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'80vh'}}>
-            <div style={{width:'min(100%,400px)',display:'flex',flexDirection:'column',margin:'auto',alignItems:'center',justifyContent:'space-evenly',border:'solid 2px red',margin:'10px',padding:'20px'}}>
+            <div style={{width:'min(100%,400px)',display:'flex',flexDirection:'column',margin:'auto',alignItems:'center',justifyContent:'space-evenly',border:'solid 2px grey',margin:'10px',padding:'20px'}}>
                 <h1>Counter</h1>
                 <h1>{count}</h1>
                 <div style={{ display: 'flex', justifyContent: 'space-evenly',width:'100%',margin:'10px'}}>
-                    <button onClick={()=>dispatch(decrement())}>Decrement</button>
-                    <button onClick={()=>dispatch(reset())}>Reset</button>
-                    <button onClick={()=>dispatch(increment())}>Increment</button>
+                    <button onClick={()=>dispatch(decrement())} className='btn btn-warning'>Decrement</button>
+                    <button onClick={()=>dispatch(reset())} className='btn btn-danger'>Reset</button>
+                    <button onClick={()=>dispatch(increment())} className='btn btn-warning'>Increment</button>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'space-evenly',width:'100%' }}>
-                    <input type='number' onChange={e=>setamount(e.target.value)} value={amount} placeholder='Enter the amount to increment' className='form-controll'/>
-                    <button onClick={handleIncrementByAmount}>Increment by amount</button>
+                    <input type='number' onChange={e=>setamount(e.target.value)} value={amount} placeholder='Enter the amount to increment' className='form-controll m-2'/>
+                    <button onClick={handleIncrementByAmount} className='btn btn-warning p-1  m-2'>Increment by amount</button>
                 </div>
             </div>
         </div>
